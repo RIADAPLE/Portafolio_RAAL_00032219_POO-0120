80de8cdf5b0c21c5cb45049193fb7ce3512528f9
RIADAPLE <00032219@uca.edu.sv>
2020-07-01 18:44:21 -0600

    subiendo el preparcial

---
+++ semana15_Preparcial Final/Práctico/Labo 05/CProxy.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Labo_05
+{
+    public class CProxy
+    {
+
+        public interface ISujeto
+        {
+            void Pejercicio01(int n);
+            void Pejercicio02(string palabrita);
+            void Pejercicio03(int x, int n, bool f);
+            void Pejercicio04(string b);
+            void Pejercicio05(int n1);
+        }
+
+        public class ProxySencillo : ISujeto
+        {
+            private Labo05 CLabo;
+
+            public void Pejercicio01(int n)
+            {
+                if (CLabo == null)
+                {
+                    CLabo = new Labo05();
+                }
+
+                CLabo.ejercicio01(n);
+            }
+
+            public void Pejercicio02(string palabrita)
+            {
+                CLabo.ejercicio02(palabrita);
+            }
+
+            public void Pejercicio03(int x, int n, bool f)
+            {
+                CLabo.ejercicio03(x, n, f);
+            }
+
+            public void Pejercicio04(string b)
+            {
+                CLabo.ejercicio04(b);
+            }
+
+            public void Pejercicio05(int n1)
+            {
+                CLabo.ejercicio05(n1);
+            }
+
+            private class Labo05
+            {
+                public void ejercicio01(int n)
+                {
+                    int x = 1;
+                    int y = 0;
+                    int total = 0;
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (i == 2)
+                        {
+                            Console.WriteLine("1");
+                        }
+                        else
+                        {
+                            Console.WriteLine(total);
+                            total = x + y;
+                            y = x;
+                            x = total;
+                        }
+                    }
+                }
+
+                public void ejercicio02(string palabrita)
+                {
+                    bool palindrome = true;
+                    int n = palabrita.Length;
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (palabrita[i] == palabrita[n - i - 1])
+                        {
+                        }
+                        else
+                        {
+                            palindrome = false;
+                            break;
+                        }
+                        Console.WriteLine(palindrome);
+                    }
+
+                    if (palindrome)
+                        Console.WriteLine("Es palíndrome!");
+                    else
+                        Console.WriteLine("No es palíndrome!");
+                }
+
+                public void ejercicio03(int x, int n, bool f)
+                {
+                    if (f == true && x == n)
+                    {
+                        Console.WriteLine(n);
+                        f = false;
+                        ejercicio03(x,n,f);
+                    }
+                    else if (f == true)
+                    {
+                        Console.WriteLine(x);
+                        x++;
+                        ejercicio03(x,n,f);
+                    }
+                    else if (f == false)
+                    {
+                        if (x == 1)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            x--;
+                            Console.WriteLine(x);
+                            ejercicio03(x, n, f);
+                        }
+                    }
+                    else
+                        return;
+                }
+                
+                public void ejercicio04(string b)
+                {
+                    for (int i = 0; i < b.Length; i++)
+                    {
+                        if (b[i].Equals('a'))
+                        {
+                            Console.Write("b");
+                        }
+                        else
+                        {
+                            Console.Write(b[i]);
+                        }
+                    }
+                }
+
+                public void ejercicio05(int n1)
+                {
+                    List<char> lista = new List<char>();
+                    char c;
+                    for (int i = 0; i < n1; i++)
+                    {Console.WriteLine("Letra "+(i+1)+": ");
+                        c = Convert.ToChar(Console.ReadLine());
+                        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U'){
+                            lista.Add(c);
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Las vocales que se ingresaron fueron las siguientes:");
+                    foreach (char caracter in lista)
+                    {
+                        Console.WriteLine(caracter);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ semana15_Preparcial Final/Práctico/Labo 05/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Labo_05
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            bool continuar = true;
+            CProxy.ISujeto miejercicio = new CProxy.ProxySencillo();
+
+            do{
+                Console.Write(menu());
+                int opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Write("Introduzca cuantos términos de fibonacci quiere que salgan: ");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        miejercicio.Pejercicio01(n);
+                        break;
+                    case 2:
+                        Console.Write("Introduzca una palabra: ");
+                        String palabrita = Console.ReadLine();
+                        palabrita=palabrita.ToLower();
+                        miejercicio.Pejercicio02(palabrita);
+                        break;
+                    case 3:
+                        Console.WriteLine("Digite hasta que número se repetirá: ");
+                        int q = Convert.ToInt32(Console.ReadLine());
+                        bool f=true;
+                        miejercicio.Pejercicio03(1,q,f);
+                        break;
+                    case 4:
+                        Console.Write("Introduzca una palabra: ");
+                        String palabra = Console.ReadLine();
+                        miejercicio.Pejercicio04(palabra);
+                        break;
+                    case 5:
+                        Console.WriteLine("Digite cuantas letras va a introducir");
+                        int n1 = Convert.ToInt32(Console.ReadLine());
+                        miejercicio.Pejercicio05(n1);
+                        break;
+                    case 0:
+                        continuar = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opcion errónea!");
+                        break;
+                }
+            } while (continuar);
+        }
+        
+        static string menu(){
+            return "\nBienvenido:\n" +
+                   "1) Ejercicio 1.\n" +
+                   "2) Ejercicio 2.\n" +
+                   "3) Ejercicio 3.\n" +
+                   "4) Ejercicio 4.\n" +
+                   "5) Ejercicio 5.\n" +
+                   "0) Salir.\n" +
+                   "Opción elegida: ";
+        }
+    
+    }
+}
